8c7c8cd3e5602965b1cdaa631ad72fc1949ba31f
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2022-12-10 21:55:21 +0530

    single global hook (untested)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './useConnectWallet';
-export * from './useMetaMask';
 export * from './useW3b';
 export * from './web3store';
 export * from './useContract';
--- src/useW3b.ts
+++ src/useW3b.ts
@@ -4,8 +4,21 @@ import { ethers } from 'ethers';
 import shallow from 'zustand/shallow';
 
 export const useW3b = () => {
-  const [connectedAccount, setBalance, provider, setProvider] = useWeb3Store(
-    (state: Web3Actions & Web3State) => [
+  const [
+    isInstalledWallet,
+    setConnectedAccount,
+    setIsInstalledWallet,
+    setIsConnected,
+    connectedAccount,
+    setBalance,
+    provider,
+    setProvider,
+  ] = useWeb3Store(
+    (state: Web3State & Web3Actions) => [
+      state.isInstalledWallet,
+      state.setConnectedAccount,
+      state.setIsInstalledWallet,
+      state.setIsConnected,
       state.connectedAccount,
       state.setBalance,
       state.provider,
@@ -14,10 +27,51 @@ export const useW3b = () => {
     shallow
   );
 
-  const BalanceContract = useCallback(async () => {
+  const getBalance = useCallback(async () => {
     const balance = await provider.getBalance(connectedAccount);
     setBalance(ethers.utils.formatEther(balance));
   }, [connectedAccount, provider]);
+
+  const onChangeAccounts = async () => {
+    window.ethereum.on('accountsChanged', function(accounts: string[]) {
+      if (accounts && accounts.length) {
+        setConnectedAccount(accounts[0]);
+        setIsConnected(true);
+      } else {
+        setConnectedAccount(null);
+        setIsConnected(false);
+      }
+    });
+  };
+
+  const onChangeChain = async () => {
+    window.ethereum.on('chainChanged', function(_chainId: string) {
+      console.log('chainChanged:', parseInt(_chainId));
+      window.location.reload();
+    });
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    const accounts = await window.ethereum.request({
+      method: 'eth_accounts',
+    });
+    if (accounts && accounts.length) {
+      setConnectedAccount(accounts[0]);
+      setIsConnected(true);
+    } else {
+      setConnectedAccount(null);
+      setIsConnected(false);
+      console.log('No accounts found');
+    }
+  };
+
+  useEffect(() => {
+    setIsInstalledWallet(!!window.ethereum);
+  }, []);
+
+  if (!isInstalledWallet) {
+    throw new Error('No ethereum object');
+  }
 
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -25,6 +79,12 @@ export const useW3b = () => {
   }, []);
 
   useEffect(() => {
-    if (connectedAccount && provider) BalanceContract();
-  }, [connectedAccount, provider, BalanceContract]);
+    if (connectedAccount && provider) getBalance();
+  }, [getBalance]);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    onChangeAccounts();
+    onChangeChain();
+  }, []);
 };
